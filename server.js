86ba41e39d34bce372da07d87c277d1d379b0991
t-okamoto <t-okamoto@CN23-R-0159.local>
2025-10-02 12:38:52 +0900

    ヘッダー認証機能の追加

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 import Fastify from 'fastify';
 
-const fastify = Fastify({
-  logger: true
+const fastify = Fastify({ logger: true })
+
+// ヘッダー認証の追加
+fastify.addHook('onRequest' async (request, reply) => {
+  const apikey = request.headers['x-api-key']
+
+  if (apikey !== '123' ) {
+    reply.code(401).send({error: 'アクセス許可がありません'})
+  }
 })
 
+// 認証が通ればここに
 fastify.get('/', async function handler (request, reply){
-  return { message: 'Dockerテスト' };
+  return { message: 'Dockerテスト-ヘッダー認証通過' };
 })
 
 try {
